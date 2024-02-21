9fc8a3a55783083b34ac5befdbc374f8c674be9e
realmbs <119774290+realmbs@users.noreply.github.com>
2024-02-20 19:19:57 -0600

    implemented basic calculator functionality in script.ts

---
+++ script.ts
@@ -0,0 +1,17 @@
+interface Calculator {
+  add(a: number, b: number): number;
+  subtract(a: number, b: number): number;
+  multiply(a: number, b: number): number;
+  divide(a: number, b: number): number;
+  exponent(a: number, b: number): number;
+  sqrt(a: number): number;
+}
+
+const calculator: Calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+  exponent: (a, b) => a ** b,
+  sqrt: (a) => Math.sqrt(a)
+}
