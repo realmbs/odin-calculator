6e544cceb9532a156e77992e1807139d55fc5437
realmbs <119774290+realmbs@users.noreply.github.com>
2024-02-21 17:25:17 -0600

    working on logic

--- script.js
+++ script.js
@@ -8,13 +8,80 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     return to.concat(ar || Array.prototype.slice.call(from));
 };
 var calculator = {
-    '+': function (a, b) { return a + b; },
-    '-': function (a, b) { return a - b; },
-    '*': function (a, b) { return a * b; },
-    '/': function (a, b) { return a / b; },
-    '%': function (a, b) { return a * b / 100; },
-    '±': function (a) { return -a; },
+    add: function (a, b) { return a + b; },
+    subtract: function (a, b) { return a - b; },
+    multiply: function (a, b) { return a * b; },
+    divide: function (a, b) { return a / b; },
+    percent: function (a, b) { return (a * b) / 100; },
+    signChange: function (a) { return -a; }
 };
+var calculatorDisplay = document.querySelector('.output__text');
 var calculatorButtons = __spreadArray([], document.querySelectorAll('.button'), true);
+var modifyButtons = __spreadArray([], document.querySelectorAll('.modify__button'), true);
+var numberButtons = __spreadArray([], document.querySelectorAll('.number__button'), true);
 var operationButtons = __spreadArray([], document.querySelectorAll('.operation__button'), true);
-var display = document.querySelector('.output__text');
+var clearButton = document.querySelector('.clear__button');
+var equalsButton = document.querySelector('.equals__button');
+var result = [];
+clearButton === null || clearButton === void 0 ? void 0 : clearButton.addEventListener('click', function () {
+    result.length = 0;
+    console.log(result);
+});
+numberButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        var buttonValue = button.textContent;
+        if (result.length === 0 || typeof result[result.length - 1] === 'string') {
+            result.push(Number(buttonValue));
+        }
+        else {
+            var lastNumber = result[result.length - 1];
+            result[result.length - 1] = Number("".concat(lastNumber).concat(buttonValue));
+        }
+        console.log(result);
+    });
+});
+operationButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        var buttonValue = button.textContent;
+        result.push(buttonValue);
+        console.log(result);
+    });
+});
+modifyButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        var buttonValue = button.textContent;
+        result.push(buttonValue);
+        console.log(result);
+    });
+});
+equalsButton === null || equalsButton === void 0 ? void 0 : equalsButton.addEventListener('click', function () {
+    var firstOperand = result[0];
+    var operator = result[1];
+    var secondOperand = result[2];
+    if (firstOperand && operator && secondOperand) {
+        var firstNumber = Number(firstOperand);
+        var secondNumber = Number(secondOperand);
+        var resultValue = 0;
+        switch (operator) {
+            case '+':
+                resultValue = calculator.add(firstNumber, secondNumber);
+                break;
+            case '-':
+                resultValue = calculator.subtract(firstNumber, secondNumber);
+                break;
+            case '×':
+                resultValue = calculator.multiply(firstNumber, secondNumber);
+                break;
+            case '÷':
+                resultValue = calculator.divide(firstNumber, secondNumber);
+                break;
+            case '%':
+                resultValue = calculator.percent(firstNumber, secondNumber);
+                break;
+            default:
+                console.error('Invalid operator');
+                return;
+        }
+        console.log(resultValue);
+    }
+});
--- script.ts
+++ script.ts
@@ -1,21 +1,94 @@
-interface Operator {
-  '+': (a: number, b: number) => number;
-  '-': (a: number, b: number) => number;
-  '*': (a: number, b: number) => number;
-  '/': (a: number, b: number) => number;
-  '%': (a: number, b: number) => number;
-  '±': (a: number) => number;
+interface Calculator {
+  add: (a: number, b: number) => number;
+  subtract: (a: number, b: number) => number;
+  multiply: (a: number, b: number) => number;
+  divide: (a: number, b: number) => number;
+  percent: (a: number, b: number) => number;
+  signChange: (a: number) => number;
 }
 
-const calculator: Operator = {
-  '+': (a, b) => a + b,
-  '-': (a, b) => a - b,
-  '*': (a, b) => a * b,
-  '/': (a, b) => a / b,
-  '%': (a, b) => a * b / 100,
-  '±': (a) => -a,
+const calculator: Calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+  percent: (a, b) => (a * b) / 100,
+  signChange: (a) => -a
 }
 
+const calculatorDisplay = document.querySelector('.output__text');
 const calculatorButtons = [...document.querySelectorAll('.button')];
+const modifyButtons = [...document.querySelectorAll('.modify__button')];
+const numberButtons = [...document.querySelectorAll('.number__button')];
 const operationButtons = [...document.querySelectorAll('.operation__button')];
-const display = document.querySelector('.output__text') as HTMLDivElement;
+const clearButton = document.querySelector('.clear__button');
+const equalsButton = document.querySelector('.equals__button');
+
+const result: (number | string | null)[] = [];
+
+clearButton?.addEventListener('click', () => {
+  result.length = 0;
+  console.log(result);
+})
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const buttonValue = button.textContent;
+    if (result.length === 0 || typeof result[result.length - 1] === 'string') {
+      result.push(Number(buttonValue));
+    } else {
+      const lastNumber = result[result.length - 1];
+      result[result.length - 1] = Number(`${lastNumber}${buttonValue}`);
+    }
+    console.log(result);
+  })
+})
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const buttonValue = button.textContent;
+    result.push(buttonValue);
+    console.log(result);
+  })
+})
+
+modifyButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const buttonValue = button.textContent;
+    result.push(buttonValue);
+    console.log(result);
+  })
+})
+
+equalsButton?.addEventListener('click', () => {
+  const firstOperand = result[0];
+  const operator = result[1];
+  const secondOperand = result[2];
+
+  if (firstOperand && operator && secondOperand) {
+    const firstNumber = Number(firstOperand);
+    const secondNumber = Number(secondOperand);
+    let resultValue = 0;
+    switch (operator) {
+      case '+':
+        resultValue = calculator.add(firstNumber, secondNumber);
+        break;
+      case '-':
+        resultValue = calculator.subtract(firstNumber, secondNumber);
+        break;
+      case '×':
+        resultValue = calculator.multiply(firstNumber, secondNumber);
+        break;
+      case '÷':
+        resultValue = calculator.divide(firstNumber, secondNumber);
+        break;
+      case '%':
+        resultValue = calculator.percent(firstNumber, secondNumber);
+        break;
+      default:
+        console.error('Invalid operator');
+        return;
+    }
+    console.log(resultValue);
+  }
+})
