454c5c3ec1238287305228b87abf4499aac292b6
realmbs <119774290+realmbs@users.noreply.github.com>
2024-02-21 10:49:34 -0600

    starting over w calc logic

--- script.js
+++ script.js
@@ -7,70 +7,14 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     }
     return to.concat(ar || Array.prototype.slice.call(from));
 };
-var calculatorButtons = __spreadArray([], document.querySelectorAll('.button'), true);
-var display = document.querySelector('.calculator__display');
-var container = document.querySelector('.calculator__container');
 var calculator = {
-    add: function (a, b) { return a + b; },
-    subtract: function (a, b) { return a - b; },
-    multiply: function (a, b) { return a * b; },
-    divide: function (a, b) { return a / b; },
-    percent: function (a, b) { return a * b / 100; },
-    negate: function (a) { return -a; },
+    '+': function (a, b) { return a + b; },
+    '-': function (a, b) { return a - b; },
+    '*': function (a, b) { return a * b; },
+    '/': function (a, b) { return a / b; },
+    '%': function (a, b) { return a * b / 100; },
+    '±': function (a) { return -a; },
 };
-var completeOneOperation = function (a, b, operation) {
-    switch (operation) {
-        case '+':
-            return calculator.add(a, b);
-            break;
-        case '-':
-            return calculator.subtract(a, b);
-            break;
-        case '*' || 'x':
-            return calculator.multiply(a, b);
-            break;
-        case '/' || '÷':
-            return calculator.divide(a, b);
-            break;
-        case '%':
-            return calculator.percent(a, b);
-            break;
-        default:
-            return 0;
-            break;
-    }
-};
-calculatorButtons.forEach(function (button) {
-    button.addEventListener('click', function () {
-        console.log(button.textContent);
-        var buttonValue = button.textContent;
-        switch (buttonValue) {
-            case 'AC':
-                display.textContent = '0';
-                break;
-            case '±':
-                display.textContent = calculator.negate(+display.textContent).toString();
-                break;
-            case '%':
-                display.textContent = calculator.percent(+display.textContent, 100).toString();
-                break;
-            case '÷':
-                display.textContent = calculator.divide(+display.textContent, 100).toString();
-                break;
-            case '×':
-                display.textContent = calculator.multiply(+display.textContent, 100).toString();
-                break;
-            case '−':
-                display.textContent = calculator.subtract(+display.textContent, 100).toString();
-                break;
-            case '+':
-                display.textContent = calculator.add(+display.textContent, 100).toString();
-                break;
-            case '=':
-                display.textContent = completeOneOperation(+display.textContent, 100, '=').toString();
-                break;
-            default:
-                display.textContent = '0';
-        }
-    });
-});
+var calculatorButtons = __spreadArray([], document.querySelectorAll('.button'), true);
+var operationButtons = __spreadArray([], document.querySelectorAll('.operation__button'), true);
+var display = document.querySelector('.output__text');
--- script.ts
+++ script.ts
@@ -1,79 +1,21 @@
-interface Calculator {
-  add(a: number, b: number): number;
-  subtract(a: number, b: number): number;
-  multiply(a: number, b: number): number;
-  divide(a: number, b: number): number;
-  percent(a: number, b: number): number;
-  negate(a: number): number;
+interface Operator {
+  '+': (a: number, b: number) => number;
+  '-': (a: number, b: number) => number;
+  '*': (a: number, b: number) => number;
+  '/': (a: number, b: number) => number;
+  '%': (a: number, b: number) => number;
+  '±': (a: number) => number;
+}
+
+const calculator: Operator = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+  '/': (a, b) => a / b,
+  '%': (a, b) => a * b / 100,
+  '±': (a) => -a,
 }
 
 const calculatorButtons = [...document.querySelectorAll('.button')];
-const display = document.querySelector('.calculator__display') as HTMLDivElement;
-const container = document.querySelector('.calculator__container') as HTMLDivElement;
-
-const calculator: Calculator = {
-  add: (a, b) => a + b,
-  subtract: (a, b) => a - b,
-  multiply: (a, b) => a * b,
-  divide: (a, b) => a / b,
-  percent: (a, b) => a * b / 100,
-  negate: (a) => -a,
-}
-
-const completeOneOperation = (a: number, b: number, operation: string): number => {
-  switch (operation) {
-    case '+':
-      return calculator.add(a, b);
-      break;
-    case '-':
-      return calculator.subtract(a, b);
-      break;
-    case '*' || 'x':
-      return calculator.multiply(a, b);
-      break;
-    case '/' || '÷':
-      return calculator.divide(a, b);
-      break;
-    case '%':
-      return calculator.percent(a, b);
-      break;
-    default:
-      return 0;
-      break;
-  }
-}
-
-calculatorButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    console.log(button.textContent);
-    const buttonValue = button.textContent;
-    switch (buttonValue) {
-      case 'AC':
-        display.textContent = '0';
-        break;
-      case '±':
-        display.textContent = calculator.negate(+display.textContent).toString();
-        break;
-      case '%':
-        display.textContent = calculator.percent(+display.textContent, 100).toString();
-        break;
-      case '÷':
-        display.textContent = calculator.divide(+display.textContent, 100).toString();
-        break;
-      case '×':
-        display.textContent = calculator.multiply(+display.textContent, 100).toString();
-        break;
-      case '−':
-        display.textContent = calculator.subtract(+display.textContent, 100).toString();
-        break;
-      case '+':
-        display.textContent = calculator.add(+display.textContent, 100).toString();
-        break;
-      case '=':
-        display.textContent = completeOneOperation(+display.textContent, 100, '=').toString();
-        break;
-      default:
-        display.textContent = '0';
-    }
-  })
-})
+const operationButtons = [...document.querySelectorAll('.operation__button')];
+const display = document.querySelector('.output__text') as HTMLDivElement;
